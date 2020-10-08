60d6561d9382325f4278f75db0091b85dc0c0bfa
h-sigma <harsh@aka.al>
2020-10-08 18:48:45 +0530

    Added "Group" abstraction for discord lobbies.

---
+++ Assets/Scripts/DiscordCommon/Group/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscordCommon.Lobby
+{
+    /// <summary>
+    /// Wrapper over Discord Lobbies, renamed to limit confusion.
+    /// </summary>
+    public class Group
+    {
+        
+    }
+}
