45f62cd7f7716c9bb550f0b1989ad233964e2653
h-sigma <harsh@aka.al>
2020-10-25 13:07:15 +0530

    Modified activity updater.

--- Assets/Scripts/DiscordCommon/DiscordActivitySetter.cs
+++ Assets/Scripts/DiscordCommon/DiscordActivitySetter.cs
@@ -1,14 +1,22 @@
 ﻿using Discord;
 using DiscordAppLayer;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 namespace DiscordCommon
 {
     public class DiscordActivitySetter : MonoBehaviour
     {
-        [SerializeField] private bool setOnEnable = true;
+        /// <summary>
+        /// Whether to set activity whenever this gameobject is enabled.
+        /// Useful for putting this on various game screens to automatically change activity based on which
+        /// screen was last displayed.
+        /// </summary>
+        [SerializeField]
+        private bool setOnEnable = true;
 
-        [SerializeField] private SerializedDiscordActivity activity = null;
+        [Header("If null, Clear."), SerializeField]
+        private SerializedDiscordActivity activity = null;
 
         public void OnEnable()
         {
@@ -23,7 +31,14 @@ namespace DiscordCommon
             if (!DiscordApp.GetDiscordApp(out DiscordApp app)) return;
 
             var manager = app.ActivityManager;
-            manager.UpdateActivity(activity.GetActivity(), OnActivitySet);
+            if (activity != null)
+                manager.UpdateActivity(activity.GetActivity(), OnActivitySet);
+            else manager.ClearActivity((result => { Debug.Log($"Clear Activity result: {result}"); }));
+        }
+
+        public static Discord.Discord GetManager()
+        {
+            return new Discord.Discord();
         }
 
         private static void OnActivitySet(Result result)
@@ -31,6 +46,10 @@ namespace DiscordCommon
             if (result != Result.Ok)
             {
                 Debug.LogWarning($"Failed to set activity. Reason: {result}.");
+            }
+            else
+            {
+                Debug.Log($"Successfully set activity.");
             }
         }
     }
--- Assets/Scripts/DiscordCommon/SerializedDiscordActivity.cs
+++ Assets/Scripts/DiscordCommon/SerializedDiscordActivity.cs
@@ -8,28 +8,63 @@ namespace DiscordCommon
     [CreateAssetMenu(menuName = "Discord/Serialized Activity")]
     public class SerializedDiscordActivity : ScriptableObject
     {
-        [SerializeField]
+        /// <summary>
+        /// Not displayed to the user.
+        /// </summary>
+        [SerializeField, Header("Basic Description")]
         private string activityName = string.Empty;
 
+        /// <summary>
+        /// Lighter-shade text that's meant to further explain the "State".
+        /// </summary>
         [SerializeField]
         private string details = string.Empty;
 
+        /// <summary>
+        /// Phrases like "Playing Solo", "Queueing", "Idleing", "Matchmaking", "Playing Random".
+        /// </summary>
         [SerializeField]
         private string state = string.Empty;
 
+        /// <summary>
+        /// Not required to be set explicitly. Only useful for callbacks.
+        /// </summary>
         [SerializeField]
         private ActivityType type = ActivityType.Watching;
 
+        /*     Explanation:
+         *     In discord developer portal, you can upload asset images and give them a "key".
+         *     That's the key field below. And the tooltip field is for text displayed when user hovers on image.
+         *     The activity-view has two variations for images: large and small.
+         */
+        [Header("Assets"), SerializeField]
+        private string LargeTooltipText = string.Empty;
+
+        [SerializeField]
+        private string LargeImageKey = string.Empty;
+
+        [SerializeField]
+        private string SmallTooltipText = string.Empty;
+
+        [SerializeField]
+        private string SmallImageKey = string.Empty;
+
         public Activity GetActivity()
         {
-            DiscordApp.GetDiscordApp(out var app);
             return new Activity()
             {
                 ApplicationId = WhooSettings.Instance.discordAppId,
                 Name          = activityName,
                 Details       = details,
                 State         = state,
-                Type          = type
+                Type          = type,
+                Assets = new ActivityAssets()
+                {
+                    LargeImage = LargeImageKey,
+                    SmallImage = SmallImageKey,
+                    LargeText = LargeTooltipText,
+                    SmallText = SmallTooltipText
+                }
             };
         }
     }
