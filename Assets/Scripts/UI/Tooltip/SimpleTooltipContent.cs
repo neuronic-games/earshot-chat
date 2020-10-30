f73ba753276ddee669716d3f2963cef84264a4c0
h-sigma <harsh@aka.al>
2020-10-30 05:39:04 +0530

    Added Tooltips to the app for better ux

---
+++ Assets/Scripts/UI/Tooltip/SimpleTooltipContent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class SimpleTooltipContent : TooltipContent
+    {
+        [Header("CONTENT")]
+        [TextArea]
+        public string description;
+
+        public override string GetTooltipText() => description;
+    }
+}
---
+++ Assets/Scripts/UI/Tooltip/TableTooltip.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Whoo.Views
+{
+    public class TableTooltip : TooltipContent
+    {
+        [Header("Config")]
+        public GroupView view;
+
+        [Header("Strings")]
+        public string notSeated;
+
+        public string isSeated;
+
+        public string contextMenu;
+
+        public override void Start()
+        {
+            base.Start();
+            if (view == null)
+            {
+                view = transform.parent.GetComponentInChildren<TableView>();
+            }
+        }
+
+        private static StringBuilder _sb = new StringBuilder();
+
+        public override string GetTooltipText()
+        {
+            _sb.Clear();
+            if (view.Group.LocalUser.IsSitting())
+            {
+                if (!string.IsNullOrEmpty(isSeated))
+                {
+                    _sb.AppendLine(isSeated);
+                }
+            }
+            else if (!string.IsNullOrEmpty(notSeated)) 
+                    _sb.AppendLine(notSeated);
+            
+            if (!string.IsNullOrEmpty(contextMenu)) _sb.AppendLine(contextMenu);
+            return _sb.ToString();
+        }
+    }
+}
