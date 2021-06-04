7e3d53407049fb800978771e049226f96a188481
Valya Zaitseva <val.zaitseva@gmail.com>
2021-06-04 09:49:25 +0300

    Add NetLauncher

---
+++ Assets/Scripts/Networking/NetLauncher.cs
@@ -0,0 +1,27 @@
+﻿using MLAPI;
+
+namespace Networking
+{
+    public class NetLauncher : NetworkBehaviour
+    {
+        public static void StartHost()
+        {
+            NetworkManager.Singleton.StartHost();
+            //SetPlayerInactive();
+        }
+
+        public static void StartClient()
+        {
+            NetworkManager.Singleton.StartClient();
+            //SetPlayerInactive();
+        }
+
+        private void SetPlayerInactive()
+        {
+            if (IsOwner)
+            {
+                // find a player and deactivate it
+            }
+        }
+    }
+}
