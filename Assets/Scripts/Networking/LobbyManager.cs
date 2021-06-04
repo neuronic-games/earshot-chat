7f4f545186e7d41b3d83e7e533f976594e3cdc90
Valya Zaitseva <val.zaitseva@gmail.com>
2021-06-04 16:34:51 +0300

    Add LobbyManager and logic for collecting AvatarPlayers

---
+++ Assets/Scripts/Networking/LobbyManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Networking
+{
+    public class LobbyManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject avatarViewPrefab;
+        [SerializeField] private Transform parent;
+        [SerializeField] private NetLauncher netLauncher;
+        
+        private Text _name;
+        private GameObject _avatarImage;
+
+        private void Start()
+        {
+            netLauncher.AddedAvatarPlayer += PopulateLobby;
+        }
+
+        private void OnDestroy()
+        {
+            netLauncher.AddedAvatarPlayer -= PopulateLobby;
+        }
+
+        private void PopulateLobby(List<AvatarPlayer> avatarPlayers)
+        {
+            Debug.Log("Avatar players count " + avatarPlayers.Count);
+        }
+    }
+}
--- Assets/Scripts/Networking/NetLauncher.cs
+++ Assets/Scripts/Networking/NetLauncher.cs
@@ -1,19 +1,27 @@
-﻿using MLAPI;
+﻿using System;
+using System.Collections.Generic;
+using MLAPI;
 
 namespace Networking
 {
     public class NetLauncher : NetworkBehaviour
     {
+        private static readonly List<AvatarPlayer> _avatarPlayers = new List<AvatarPlayer>();
+
+        public Action<List<AvatarPlayer>> AddedAvatarPlayer;
+        
         public static void StartHost()
         {
             NetworkManager.Singleton.StartHost();
             //SetPlayerInactive();
+            //AddPlayerToList();
         }
 
         public static void StartClient()
         {
             NetworkManager.Singleton.StartClient();
             //SetPlayerInactive();
+            //AddPlayerToList();
         }
 
         private void SetPlayerInactive()
@@ -23,6 +31,14 @@ namespace Networking
                 var clientId = NetworkManager.Singleton.LocalClientId;
                 NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.gameObject.SetActive(false);
             }
+        }
+
+        private void AddPlayerToList()
+        {
+            var clientId = NetworkManager.Singleton.LocalClientId;
+            var avatarPlayerObject = NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.gameObject.GetComponent<AvatarPlayer>();
+            _avatarPlayers.Add(avatarPlayerObject);
+            AddedAvatarPlayer?.Invoke(_avatarPlayers);
         }
     }
 }
