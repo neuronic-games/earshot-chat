87436143c3f737176faa77a493ed5a76bedf9357
Valya Zaitseva <val.zaitseva@gmail.com>
2021-06-14 12:05:11 +0300

    Set avatar for AvatarViewInLobby

--- Assets/Scripts/Networking/AvatarViewInLobby.cs
+++ Assets/Scripts/Networking/AvatarViewInLobby.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Networking
 {
     public class AvatarViewInLobby : MonoBehaviour
     {
-        
+        [SerializeField] private Image iconImage;
+
+        private void Start()
+        {
+            iconImage.sprite = UserDTO.AvatarSprite;
+        }
     }
 }
