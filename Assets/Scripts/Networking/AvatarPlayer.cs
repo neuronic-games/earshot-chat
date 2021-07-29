6acf156c2c43686e4bbb590370c5577d6aa7b6b7
Shuyning <shuyning00@gmail.com>
2021-07-29 10:28:14 +0300

    Revert "Click left mouse button for change position."
    This reverts commit d4b6b5b3d31022704b70db412c127f8375aba4e5.

--- Assets/Scripts/Networking/AvatarPlayer.cs
+++ Assets/Scripts/Networking/AvatarPlayer.cs
@@ -26,7 +26,6 @@ namespace Networking
                 new NetworkVariableSettings() {WritePermission = NetworkVariablePermission.OwnerOnly}, Color.cyan);
         
         private TextMeshPro tmp;
-        private RaycastHit2D hit;
 
         public Sprite AvatarSprite => iconSprite.sprite;
         public Color BackgroundColor => background.color;
@@ -52,8 +51,6 @@ namespace Networking
             {
                 var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0);
                 transform.position += move * 5f * Time.deltaTime;
-
-                SetAvatarPlayerPosition();
             }
 
             SetUserName();
@@ -75,15 +72,6 @@ namespace Networking
         {
             Sprite sprite = avatarCollection.AvatarVariants.FirstOrDefault(a => a.name == avatarIconName.Value);
             iconSprite.sprite = sprite;
-        }
-
-        private void SetAvatarPlayerPosition()
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-            }
         }
     }
 }
